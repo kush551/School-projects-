d28dbee5a5ca7e76987c36e8c5613176c3623b0c
Kushagra Kumar <79048366+kush551@users.noreply.github.com>
2023-02-05 21:25:24 +0530

    Add files via upload

---
+++ Table calculator.py
@@ -0,0 +1,31 @@
+num =int(input("Please Enter a Number ;"))
+
+a = num*1
+print(str(num)+" X 1 ="+str(a))
+
+a = num*2
+print(str(num)+" X 2 ="+str(a))
+
+a = num*3
+print(str(num)+" X 3 ="+str(a))
+
+a = num*4
+print(str(num)+" X 4 ="+str(a))
+
+a = num*5
+print(str(num)+" X 5 ="+str(a))
+
+a = num*6
+print(str(num)+" X 6 ="+str(a))
+
+a = num*7
+print(str(num)+" X 7 ="+str(a))
+
+a = num*8
+print(str(num)+" X 8 ="+str(a))
+
+a = num*9
+print(str(num)+" X 9 ="+str(a))
+
+a = num*10
+print(str(num)+" X 10 ="+str(a))
