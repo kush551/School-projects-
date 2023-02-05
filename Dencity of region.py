58e3843ec34ce4b00f5e7ab812d356c7f33143af
Kushagra Kumar <79048366+kush551@users.noreply.github.com>
2023-02-05 22:10:02 +0530

    Add files via upload

---
+++ Dencity of region.py
@@ -0,0 +1,6 @@
+population = int(input("Enter the population of the area :"))
+area = float(input("Enter the area of the region: "))
+
+density = population / area
+
+print("Density of the region:", density)
---
+++ quotient and reminder finder.py
@@ -0,0 +1,8 @@
+a =int(input("Enter the first number :"))
+b =int(input("Enter the second number :"))
+
+c = a//b
+d = a%b
+
+print("The quotient of", a, "and", b, "is", c)
+print("The remainder of", a, "and", b, "is", d)
